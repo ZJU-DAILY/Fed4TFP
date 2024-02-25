fb2f67d89c32aec5d04f9fc5e246961e22281d1d
zengzhihao12138 <zengzhihao@zju.edu.cn>
2024-02-25 10:58:20 +0800

    init

---
+++ core/configs/cfg_fedtfp.py
@@ -0,0 +1,12 @@
+from federatedscope.core.configs.config import CN
+from federatedscope.register import register_config
+
+
+def extend_fedtfp_cfg(cfg):
+    cfg.fedtfp = CN()
+    cfg.fedtfp.mg_use = False
+    cfg.fedtfp.tm_use = False
+    cfg.fedtfp.time_window_num = 5
+
+
+register_config("fedtfp", extend_fedtfp_cfg)
